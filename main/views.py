aad94af6208818f5a8f1ad360bcbf8706aecb734
Ilya <ilyapravv@gmail.com>
2025-04-10 21:50:10 +0700

    Sets url`s ways for about us and home page

--- main/views.py
+++ main/views.py
@@ -3,10 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Welcome'
+        'title': 'Equip - Главная страница',
+        'content': "Equipment app for SibSUTIS" 
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
     return HttpResponse('about')
+
+def about(request):
+    context = {
+        'title': 'Equip - About us ',
+        'content': "About us",
+        'text_on_page': "Данное приложение разработано в рамках ВКР Правиловым Ильёй студентом группы ИВ-121 для кафедры ВС"
+    }
+    return render(request, 'main/about.html', context)
+
