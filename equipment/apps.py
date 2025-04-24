c8e53d4312012bd3804a65e694a8889f1e06df46
Ilya <ilyapravv@gmail.com>
2025-04-24 23:09:47 +0700

    Some changes on admin panel

--- equipment/apps.py
+++ equipment/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EquipmentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'equipment'
+    verbose_name = 'Оборудование'
--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from equipment.models import Categories
+
 def index(request):
+
+    categories = Categories.objects.all()
+
+
     context = {
         'title': 'Equip - Главная страница',
-        'content': "Equipment app for SibSUTIS" 
+        'content': "Equipment app for SibSUTIS", 
+        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
