c9264fca594d7f9b8efe788ed75935d59b3c3cf3
Ilya <ilyapravv@gmail.com>
2025-04-29 23:51:29 +0700

    Add search from id

--- equipment/urls.py
+++ equipment/urls.py
@@ -6,5 +6,6 @@ app_name = 'equipment'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
+    path('search/', views.catalog, name='search'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
---
+++ equipment/utils.py
@@ -0,0 +1,10 @@
+
+
+from equipment.models import Products
+
+
+def q_search(query):
+    if query.isdigit() and len(query) <=5:
+        return Products.objects.filter(id=int(query))
+    
+    
