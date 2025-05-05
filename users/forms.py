dcc5b7611f9995aafe5a9b294c4987149c4a9737
Ilya <ilyapravv@gmail.com>
2025-05-05 07:40:43 +0700

    Added registration function on web-app, and reg view

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,17 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
 
-    username = forms.CharField()
-    password = forms.CharField()
-    
     class Meta:
         model = User
         fields = ['username', 'password']
+
+    username = forms.CharField()
+    password = forms.CharField()
+    
         
     # username = forms.CharField(
     #     label = 'Имя',
@@ -24,3 +25,23 @@ class UserLoginForm(AuthenticationForm):
     #                                       'class': 'form-control',
     #                                       'placeholder': 'Введите ваш пароль'})
     # )
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+    
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
