da1cd73732aea1faed6b299d2810dcf8eb8587e3
Ilya <ilyapravv@gmail.com>
2025-05-15 00:25:07 +0700

    Create and update carts(tables in future), add product working correct, 3x view carts

--- equipment/models.py
+++ equipment/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -19,6 +20,7 @@ class Products(models.Model):
         description = models.TextField(blank=True, null=True, verbose_name='Описание')
         image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
         price = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name='Цена')
+        discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в %')
         quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
         category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
@@ -32,5 +34,14 @@ class Products(models.Model):
         def __str__(self):
             return f'{self.name} Количество - {self.quantity}'
         
+        def get_absolute_url(self):
+            return reverse("catalog:product", kwargs={"product_slug": self.slug})
+        
         def display_id(self):
             return f"{self.id:05}"
+        
+        def sell_price(self):
+             if self.discount:
+                 return round(self.price - self.price * self.discount / 100, 2)
+             
+             return self.price
