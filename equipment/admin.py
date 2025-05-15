189b840930239c35ec1ebe7457965f64af26b78b
Ilya <ilyapravv@gmail.com>
2025-05-15 20:52:02 +0700

    Django Admin panel was setup and upgrade

--- equipment/admin.py
+++ equipment/admin.py
@@ -9,9 +9,21 @@ from equipment.models import Categories, Products
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-
+    list_display = ['name']
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    
+    list_display = ['name', 'category', 'price', 'quantity', 'display_id']
+    list_editable = ['price', 'quantity']
+    search_fields = ['name', 'description']
+    list_filter = ['category', 'quantity']
+    fields = [
+        'name',
+        'category',
+        'slug',
+        'description',
+        'image',
+        ('price','discount'),
+        'quantity'
+    ]
