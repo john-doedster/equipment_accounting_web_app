75177e57aa9c88fcaf4a84af6ecbbe1123323994
Ilya <ilyapravv@gmail.com>
2025-05-15 20:12:45 +0700

    Sessions for anonymous users add

--- carts/views.py
+++ carts/views.py
@@ -24,6 +24,19 @@ def cart_add(request):
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
 
+    #добавление товара в корзину для анонимного пользователя
+    else:
+        carts = Cart.objects.filter(session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(session_key=request.session.session_key, product=product, quantity=1)
+
+
         user_cart = get_user_carts(request)
         cart_items_html = render_to_string(
             "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
@@ -34,19 +47,6 @@ def cart_add(request):
         }
 
         return JsonResponse(response_data)
-
-    # else:
-    #     carts = Cart.objects.filter(
-    #         session_key=request.session.session_key, product=product)
-
-    #     if carts.exists():
-    #         cart = carts.first()
-    #         if cart:
-    #             cart.quantity += 1
-    #             cart.save()
-    #     else:
-    #         Cart.objects.create(
-    #             session_key=request.session.session_key, product=product, quantity=1)
     
     # user_cart = get_user_carts(request)
     # cart_items_html = render_to_string(
